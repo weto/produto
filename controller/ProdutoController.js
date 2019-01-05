6c3db1e35937853afd07cb6f57623f9a685c532d
weto <weto.jc@gmail.com>
2019-01-05 17:28:29 -0200

    Update ProdutoController.js

--- controller/ProdutoController.js
+++ controller/ProdutoController.js
@@ -3,11 +3,23 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var httpStatus = require('http-status');
 
+const produtos = require('./../produtos.json')
+
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 router.get('/', function (req, res) {
-  res.status(httpStatus.OK).json({ status: httpStatus.OK, produtos: [] });
+  res.status(httpStatus.OK).json({ status: httpStatus.OK, produtos: produtos });
 });
+
+router.get('/:idProduto', function (req, res) {
+
+  const find = produtos.find(produto => {
+    return produto.id = req.params.idProduto;
+  })
+
+  res.status(httpStatus.OK).json({ status: httpStatus.OK, produtos: find });
+});
+
 
 module.exports = router;
