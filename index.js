bd120c849ba4ff6d6d2993c83a24e50d9ac878bb
Weto <weto.jc@gmail.com>
2019-01-05 17:40:30 -0200

    Add dados de mock

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var i18n = require('./config/i18n')();
 var app = require('./app');
 var port = 3000;
 
+const produtos = require('produtos.json');
+
 i18n.init;
 console.log(i18n.__("versao"));
 var server = app.listen(port, function() {
+	console.log('Produtos: ', produtos);
 	console.log(i18n.__mf("msg_versao", {versao: i18n.__("versao")}));
 	console.log(i18n.__mf("msg_started_api", {port: port}));
 });
