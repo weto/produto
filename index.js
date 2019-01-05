23478364784ccd1e2bc62d9b801e71b2ae978592
weto <weto.jc@gmail.com>
2019-01-05 18:39:57 -0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const produtos = require('./produtos.json');
 i18n.init;
 console.log(i18n.__("versao"));
 var server = app.listen(port, function() {
-	console.log('Produtos: ', produtos);
 	console.log(i18n.__mf("msg_versao", {versao: i18n.__("versao")}));
 	console.log(i18n.__mf("msg_started_api", {port: port}));
 });
